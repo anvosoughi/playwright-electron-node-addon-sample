b38eb86dc092cf0c88f302c4922eb724770ffd05
Abtin Naseh <abtin.nvosoughi@alianza.com>
2022-03-09 13:09:04 -0800

    add logs for native

--- electron/preload.js
+++ electron/preload.js
@@ -3,10 +3,14 @@ const PLATFORM = require("os").platform();
 
 const loadLib = () => {
   if (PLATFORM === "win32") {
-    return require(`./libs/Test_Module_Windows.node`);
+		console.log('loading windows lib');
+    const test = require(`./libs/Test_Module_Windows.node`);
+		console.log('found windows lib');
+		return test;
   }
 
-  return require(`./libs/Test_Module_Mac.node`);
+	// console.log('should not get here', test);
+  // return require(`./libs/Test_Module_Mac.node`);
 };
 
 contextBridge.exposeInMainWorld("helloWorld", {
--- native/module.cpp
+++ native/module.cpp
@@ -1,15 +1,28 @@
 #include <napi.h>
 
 #include <chrono>
-#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <map>
 #include <thread>
 
+std::ofstream logFile("native.log");
+std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
+std::string logTime()
+{
+   std::stringstream ss;
+   ss << std::this_thread::get_id();
+  return ss.str() + " : " + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start).count()) + ": ";
+}
+
 void javascript_callback(Napi::Env env, Napi::Function callback, Napi::Reference<Napi::Value>* context, int* data)
 {
+  logFile << logTime()  << "javascript_callback env: " << env << " callback: " << callback << std::endl;
   if (nullptr != env && nullptr != callback)
   {
+    logFile << logTime() << "javascript_callback start callback.Call(" << env << ", " << *data << ")"<< std::endl;
     callback.Call(context->Value(), { Napi::Number::New(env, *data) });
+    logFile << logTime() << "javascript_callback end callback.Call" << std::endl;
   }
 }
 
@@ -24,18 +37,28 @@ void callbackThreadFunc()
   {
     if (sendCallback)
     {
+      logFile << logTime() << "Sending callback" << std::endl;
       int* value = new int(gResult++);
       for(auto it = gCallbacks.begin(); it != gCallbacks.end(); ++it)
       {
+        logFile << logTime() << "Start NonBlockingCall" << std::endl;
         it->second.NonBlockingCall(value);
+        logFile << logTime() << "End NonBlockingCall" << std::endl;
       }
     }
+    else
+    {
+      logFile << logTime() << "Not sending callback" << std::endl;
+    }
+    logFile << logTime() << "Start sleep 1 second" << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    logFile << logTime() << "End sleep" << std::endl;
   }
 }
 
 Napi::Value add_callback(const Napi::CallbackInfo& info)
 {
+   logFile << logTime() << "add_callback start" << std::endl;
    Napi::Env env = info.Env();
 
    if (info.Length() < 1)
@@ -65,11 +88,13 @@ Napi::Value add_callback(const Napi::CallbackInfo& info)
          delete ctx;
       }
    );
+   logFile << logTime() << "add_callback end" << std::endl;
    return Napi::Number::New(env, callbackId);
 }
 
 Napi::Value remove_callback(const Napi::CallbackInfo& info)
 {
+   logFile << logTime() << "remove_callback start" << std::endl;
    Napi::Env env = info.Env();
 
    if (info.Length() < 1)
@@ -84,10 +109,13 @@ Napi::Value remove_callback(const Napi::CallbackInfo& info)
       return env.Null();
    }
 
+   logFile << logTime() << "remove_callback pass checks" << std::endl;
+
    uint32_t handle = info[0].As<Napi::Number>();
    {
       if (gCallbacks.end() != gCallbacks.find(handle))
       {
+         logFile << logTime() << "remove_callback release it" << std::endl;
          gCallbacks[handle].Release();
          gCallbacks.erase(handle);
       }
@@ -113,13 +141,18 @@ Napi::Value query(const Napi::CallbackInfo& info)
 
 Napi::Object load_API(Napi::Env env, Napi::Object exports)
 {
+   logFile << logTime() << "load_API start" << std::endl;
+
    std::thread callbackThread(callbackThreadFunc);
    callbackThread.detach();
    exports.Set(Napi::String::New(env, "query"), Napi::Function::New(env, query));
    exports.Set(Napi::String::New(env, "add_callback"), Napi::Function::New(env, add_callback));
    exports.Set(Napi::String::New(env, "remove_callback"), Napi::Function::New(env, remove_callback));
    exports.Set(Napi::String::New(env, "trigger_callback"), Napi::Function::New(env, trigger_callback));
+   
+   logFile << logTime() << "load_API end" << std::endl;
    return exports;
+   
 }
 
 NODE_API_MODULE(NODE_GYP_MODULE_NAME, load_API)
